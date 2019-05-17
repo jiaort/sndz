594afe117c0d93460bca3f30e8a1e127230c9cda
runtao.jiao <runtao.jiao@zhengyi.tech>
2019-05-17 16:56:31 +0800

    about_view

--- manager/privilege/views.py
+++ manager/privilege/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from utils.libs.logger import SysLogger
 
 
+def about_view(request):
+    return render(request, "privilege/about_view.html")
+
+
 def login_view(request):
     """
     管理端登录
--- sndz/urls.py
+++ sndz/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from manager.privilege.views import about_view
 
 urlpatterns = [
     url(r'^api/sndz/', include("apps.urls")),
     url(r'^manager/', include("manager.urls")),
+    url(r'^$', about_view)
 ]
